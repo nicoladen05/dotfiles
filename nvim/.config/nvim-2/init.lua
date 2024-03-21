e74aa15a0cfa71df65dbedbbc46b95ef2db4ccbe
Nico <nico@pop-os.fritz.box>
2024-03-21 19:25:56 +0100

    Cleanup

--- nvim/.config/nvim-2/init.lua
+++
@@ -1,5 +0,0 @@
-require "options"
-require "lazy-pm"
-
-require "plugins.telescope"
-require "plugins.treesitter"
--- nvim/.config/nvim-2/lua/plugins/telescope.lua
+++
@@ -1,5 +0,0 @@
-local builtin = require("telescope.builtin")
-
--- Keymaps
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>ft', builtin.live_grep, {})
