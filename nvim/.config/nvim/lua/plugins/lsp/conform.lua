f34b7de469beffb0e7da69c5f1598b475f2bfffc
nicoladen05 <nicolashartmanntaba@gmail.com>
2024-04-20 09:09:15 +0200

    :sparkles: Autoformat on save

--- nvim/.config/nvim/lua/plugins/lsp/conform.lua
+++ nvim/.config/nvim/lua/plugins/lsp/conform.lua
@@ -1,9 +1,13 @@
 local status_ok, conform = pcall(require, "conform")
 if not status_ok then
-	return
+  return
 end
 
 conform.setup({
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
   formatters_by_ft = {
     lua = { "stylua" },
     python = { "isort", "black" },
