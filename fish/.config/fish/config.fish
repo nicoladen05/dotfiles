e9d293610aab85f030a357796d53639474027430
nicoladen05 <me@nicoladen.dev>
2026-07-23 09:01:40 +0000

    Sync Fish paths with Bash

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,11 @@
+set -gx BUN_INSTALL "$HOME/.bun"
+
+# Keep Fish's user paths in sync with ~/.bashrc.
+fish_add_path --global --move \
+    "$BUN_INSTALL/bin" \
+    "$HOME/.local/bin" \
+    "$HOME/.local/share/pi-node/node-v22.23.1-linux-arm64/bin"
+
 if status is-interactive
     starship init fish | source
 end
