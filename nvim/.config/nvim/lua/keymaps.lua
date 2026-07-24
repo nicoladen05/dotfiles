ef07c4366e6cb686a424b6b3fcbacc28ec288017
nicoladen05 <me@nicoladen.dev>
2026-07-24 10:13:29 +0000

    refactor neovim configuration

---
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -0,0 +1,16 @@
+-- Leader key
+vim.g.mapleader = " "
+
+-- Custom normal-mode layout: h n e i = left down up right.
+vim.keymap.set("n", "n", "j")
+vim.keymap.set("n", "e", "k")
+vim.keymap.set("n", "i", "l")
+
+-- Move displaced normal-mode commands.
+vim.keymap.set("n", "k", "i")
+vim.keymap.set("n", "j", "n")
+vim.keymap.set("n", "l", "e")
+
+-- Keep the selection active when changing indentation.
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,20 @@
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+
+-- Interface
+vim.opt.termguicolors = true
+
+-- Mouse
+vim.opt.mouse = "a"
+
+-- Persistent undo
+vim.opt.undofile = true
+
+-- Indentation
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 0 -- Default to the tabstop value.
