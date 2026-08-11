1bda7704e24c6283307cd93b7325f25ad4b47eae
nicoladen05 <me@nicoladen.dev>
2026-08-11 20:54:53 +0200

    add lazyvim config

---
+++ lazyvim/.config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      indent = { enabled = false },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = {
+        enabled = false,
+      },
+    },
+  },
+}
