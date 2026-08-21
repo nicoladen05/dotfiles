4aa83411c28e443c52babca6636574e7024f532a
nicoladen05 <me@nicoladen.dev>
2026-08-21 19:51:58 +0200

    Enable Treesitter highlighting

--- nvim/.config/nvim/lua/plugins/coding/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/coding/treesitter.lua
@@ -40,4 +40,28 @@ return {
 			"yaml",
 		},
 	},
+
+	config = function(_, opts)
+		local treesitter = require("nvim-treesitter")
+
+		treesitter.setup(opts)
+
+		vim.api.nvim_create_autocmd("FileType", {
+			group = vim.api.nvim_create_augroup("treesitter-highlight", { clear = true }),
+
+			callback = function(args)
+				local language = vim.treesitter.language.get_lang(args.match)
+
+				if not language then
+					return
+				end
+
+				local parser = vim.api.nvim_get_runtime_file("parser/" .. language .. ".*", false)[1]
+
+				if parser then
+					vim.treesitter.start(args.buf, language)
+				end
+			end,
+		})
+	end,
 }
