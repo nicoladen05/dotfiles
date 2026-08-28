bb9c777dd239528f2ee06ab1aec870f514fae8a2
Nicolas Hartmann <nicolas.hartmann@ptb.de>
2026-08-28 15:54:49 +0200

    feat(nvim): add terminal control mappings

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -3,6 +3,12 @@ return {
 	version = "*",
 	opts = {
 		direction = "horizontal",
+		on_open = function(term)
+			vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { buffer = term.bufnr, desc = "Leave terminal mode" })
+			vim.keymap.set({ "n", "t" }, "<C-c>", function()
+				vim.fn.chansend(term.job_id, "\003")
+			end, { buffer = term.bufnr, desc = "Interrupt terminal process" })
+		end,
 		open_mapping = { [[<C-/>]], [[<C-_>]] },
 		size = 15,
 	},
