c403a150f0508bf1a32380a8957b35d7d2119dc2
nicoladen05 <me@nicoladen.dev>
2026-09-02 20:21:04 +0200

    feat(nvim): add file and conflict tools

--- nvim/.config/nvim/lua/plugins/lazy.lua
+++ nvim/.config/nvim/lua/plugins/lazy.lua
@@ -31,8 +31,10 @@ require("lazy").setup({
 		require("plugins.atlas"),
 		require("plugins.colorscheme"),
 		require("plugins.fugitive"),
+		require("plugins.git-conflict"),
 		require("plugins.harpoon"),
 		require("plugins.neo-tree"),
+		require("plugins.oil"),
 		require("plugins.snacks"),
 
 		require("plugins.coding.autotag"),
---
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,13 @@
+return {
+	"stevearc/oil.nvim",
+	lazy = false,
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	keys = {
+		{
+			"-",
+			"<cmd>Oil<cr>",
+			desc = "Open Parent Directory",
+		},
+	},
+	opts = {},
+}
