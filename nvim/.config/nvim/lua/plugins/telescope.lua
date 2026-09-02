e0f859e1062dbaaf5479b1467f762013dc05e36a
Nicolas Hartmann <nicolas.hartmann@ptb.de>
2026-09-02 12:02:35 +0200

    feat(nvim): use Snacks for code actions

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -41,15 +41,4 @@ return {
 			},
 		},
 	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-
-		dependencies = {
-			"nvim-telescope/telescope.nvim",
-		},
-
-		config = function()
-			require("telescope").load_extension("ui-select")
-		end,
-	},
 }
--- nvim/.config/nvim/lua/plugins/ui/input.lua
+++ nvim/.config/nvim/lua/plugins/ui/input.lua
@@ -4,5 +4,9 @@ return {
 	priority = 1000,
 	opts = {
 		input = { enabled = true },
+		picker = {
+			enabled = true,
+			ui_select = true,
+		},
 	},
 }
