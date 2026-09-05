d967dc420cc2b03c287452c1c59ded4753a7d860
nicoladen05 <me@nicoladen.dev>
2026-09-05 20:07:02 +0200

    Configure Doom to follow Omarchy theme

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -34,7 +34,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'catppuccin)
+(setq doom-theme 'omarchy)
+(load (expand-file-name "omarchy" doom-emacs-dir))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
