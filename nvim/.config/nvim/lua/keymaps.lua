7a9f39a6cefb59f7f0308062c7e4a448c91997a1
nicoladen05 <me@nicoladen.dev>
2026-09-07 18:00:20 +0200

    Avoid tmux keybinding conflicts

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -27,10 +27,10 @@ vim.keymap.set({ "n", "v", "o" }, "J", "N")
 vim.keymap.set({ "n", "v", "o" }, "L", "E")
 
 -- Move lines down/up.
-vim.keymap.set("n", "<A-n>", "<cmd>move .+1<cr>==")
-vim.keymap.set("n", "<A-e>", "<cmd>move .-2<cr>==")
-vim.keymap.set("x", "<A-n>", ":move '>+1<cr>gv=gv")
-vim.keymap.set("x", "<A-e>", ":move '<-2<cr>gv=gv")
+vim.keymap.set("n", "<leader>mn", "<cmd>move .+1<cr>==")
+vim.keymap.set("n", "<leader>me", "<cmd>move .-2<cr>==")
+vim.keymap.set("x", "<leader>mn", ":move '>+1<cr>gv=gv")
+vim.keymap.set("x", "<leader>me", ":move '<-2<cr>gv=gv")
 
 -- Keep the selection active when changing indentation.
 vim.keymap.set("x", "<", "<gv")
