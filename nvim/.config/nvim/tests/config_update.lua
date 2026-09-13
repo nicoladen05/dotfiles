13338e1f31b9289eadb5cf2935961d101fb9bd86
Nicolas Hartmann <nicolas.hartmann@ptb.de>
2026-09-13 13:09:34 +0200

    remove tests

--- nvim/.config/nvim/tests/config_update.lua
+++
@@ -1,6 +0,0 @@
-vim.opt.runtimepath:prepend(vim.fn.getcwd())
-
-local updater = require("config_update")
-local ahead, behind = updater._parse_counts("2\t3\n")
-assert(ahead == 2 and behind == 3)
-assert(updater._parse_counts("unexpected") == nil)
