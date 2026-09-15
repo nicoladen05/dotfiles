d0d16770eca56157d336ac8c7eac0186f96fc3c5
nicoladen05 <me@nicoladen.dev>
2026-09-15 18:06:32 +0200

    nvim: pick org refile destinations with Snacks

--- nvim/.config/nvim/lua/plugins/orgmode.lua
+++ nvim/.config/nvim/lua/plugins/orgmode.lua
@@ -4,10 +4,27 @@ return {
 	ft = { "org" },
 	dependencies = {
 		{ "nvim-orgmode/org-bullets.nvim", opts = {} },
+		{
+			"nvim-orgmode/telescope-orgmode.nvim",
+			dependencies = { "folke/snacks.nvim" },
+			keys = {
+				{
+					"<leader>or",
+					function()
+						require("telescope-orgmode").refile_heading()
+					end,
+					desc = "Org Refile",
+				},
+			},
+			opts = { adapter = "snacks" },
+		},
 	},
 	opts = {
 		org_agenda_files = "~/org/*.org",
 		org_default_notes_file = "~/org/inbox.org",
+		mappings = {
+			org = { org_refile = false },
+		},
 		ui = {
 			input = { use_vim_ui = true },
 		},
