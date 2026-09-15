723e14e8fed53baf2f3d4440487cf2e7ff000cfc
nicoladen05 <me@nicoladen.dev>
2026-09-15 20:16:30 +0200

    emacs: keep native cache out of dotfiles

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,4 +1,5 @@
 ;;; init.el --- N Λ N O -*- lexical-binding: t; -*-
 
+(startup-redirect-eln-cache (expand-file-name "emacs/eln-cache" "~/.cache"))
 (add-to-list 'load-path (expand-file-name "nano-emacs" "~/.local/share"))
 (require 'nano)
